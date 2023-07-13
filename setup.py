9a0de419f30d076491f51ef9b36b65554d6fc783
“Juan <j.fernandez@fz-juelich.de>
2023-07-13 10:16:07 +0200

    First Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is a long term project for calculating the energy and carbon footprint emissions for different companies and organizations',
+    author='JuanRFernandez',
+    license='MIT',
+)
